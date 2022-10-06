2c05993558a15b624d2a41a41019562ddff300d9
bluo <bluo@monguz.hu>
2022-10-06 17:31:31 +0100

    Added animation to account for the gravity of both players

--- index.js
+++ index.js
@@ -1,73 +1,120 @@
-const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d');
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
 
 canvas.width = 1024;
 canvas.height = 576;
 
-c.fillRect(0,0,canvas.width,canvas.height)
+c.fillRect(0, 0, canvas.width, canvas.height);
 
-const gravity = 0.2
+const gravity = 0.2;
 
-class Sprite{
-    constructor({position, velocity}){
-        this.position = position
-        this.velocity = velocity
-        this.height = 150
+class Sprite {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.height = 150;
+  }
+
+  draw() {
+    c.fillStyle = "red";
+    c.fillRect(this.position.x, this.position.y, 50, this.height);
+  }
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+
+    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+      this.velocity.y = 0;
+    } else {
+      this.velocity.y += gravity;
     }
-
-    draw(){
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, 50, this.height)
-    }
-
-    update(){
-        this.draw()
-        this.position.y += this.velocity.y
-
-        if(this.position.y + this.height + this.velocity.y >= canvas.height){
-            this.velocity.y = 0
-
-        }else{
-            this.velocity.y += gravity
-        }
-    }
+  }
 }
 
+const player = new Sprite({
+  position: {
+    x: 0,
+    y: 0,
+  },
+  velocity: {
+    x: 0,
+    y: 10,
+  },
+});
 
-const player  = new Sprite({
-    position:{
-        x:0,
-        y:0
-    },
-    velocity:{
-        x:0,
-        y:10
-    }
-})
-
-const enemy  = new Sprite({
-    position:{
-        x:400,
-        y:100
-    },
-    velocity:{
-        x:0,
-        y:0
-    }
-})
-
-
+const enemy = new Sprite({
+  position: {
+    x: 400,
+    y: 100,
+  },
+  velocity: {
+    x: 0,
+    y: 0,
+  },
+});
 
 console.log(player);
 
-function animatePlayers(){
-    window.requestAnimationFrame(animatePlayers)
-    c.fillStyle = 'black'
-    c.fillRect(0,0,canvas.width,canvas.height)
-    player.update()
-    enemy.update()
+const keys = {
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+  w: {
+    pressed: false,
+  },
+};
+
+let lastkey;
+
+function animatePlayers() {
+  window.requestAnimationFrame(animatePlayers);
+  c.fillStyle = "black";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  player.update();
+  enemy.update();
+  player.velocity.x = 0;
+  if (keys.a.pressed && lastkey === "a") {
+    player.velocity.x = -1;
+  } else if (keys.d.pressed && lastkey === "d") {
+    player.velocity.x = 1;
+  }
 }
 
-animatePlayers()
+animatePlayers();
 
+window.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = true;
+      lastkey = "d";
+      break;
+    case "a":
+      keys.a.pressed = true;
+      lastkey = "a";
+      break;
+    case "w":
+      player.velocity.y = -10;
+      break;
+  }
+  console.log(event.key);
+});
 
+window.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = false;
+      break;
+    case "a":
+      keys.a.pressed = false;
+      break;
+    case "w":
+      keys.w.pressed = false;
+      break;
+  }
+  console.log(event.key);
+});
