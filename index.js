affe4090314f601507ba3a661fcf1852d275e4de
bluo <bluo@monguz.hu>
2022-10-11 13:05:48 +0100

    Implemented new functionality for characters attack to affect the health bar of each character when being attacked

--- index.js
+++ index.js
@@ -68,6 +68,14 @@ const player = new Fighter({
       framesMax: 6,
     },
   },
+  attackBox: {
+    offset: {
+      x: 100,
+      y: 50,
+    },
+    width: 160,
+    height: 50,
+  },
 });
 
 const enemy = new Fighter({
@@ -112,6 +120,14 @@ const enemy = new Fighter({
       imageSrc: "./img/kenji/Attack1.png",
       framesMax: 4,
     },
+  },
+  attackBox: {
+    offset: {
+      x: -170,
+      y: 50,
+    },
+    width: 170,
+    height: 50,
   },
 });
 
@@ -178,22 +194,36 @@ function animateGame() {
     enemy.switchSprites("fall");
   }
 
-  //detect for collision
+  //detect for collision for enemy
   if (
     rectangularCollision({ rectangle1: player, rectanlge2: enemy }) &&
-    player.isAttacking
+    player.isAttacking &&
+    player.framesCurrent === 4
   ) {
     player.isAttacking = false;
     enemy.health -= 20;
     document.querySelector("#enemyHealth").style.width = enemy.health + "%";
   }
+
+  //if player misses
+  if (player.isAttacking && player.framesCurrent === 4) {
+    player.isAttacking = false;
+  }
+
+  //detect for collision for player
   if (
     rectangularCollision({ rectangle1: enemy, rectanlge2: player }) &&
-    enemy.isAttacking
+    enemy.isAttacking &&
+    enemy.framesCurrent === 2
   ) {
     enemy.isAttacking = false;
     player.health -= 20;
     document.querySelector("#playerHealth").style.width = player.health + "%";
+  }
+
+  //if enemy misses
+  if (enemy.isAttacking && enemy.framesCurrent === 2) {
+    enemy.isAttacking = false;
   }
 
   //End game based on health
